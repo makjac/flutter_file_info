87acaacb7dadc24fdcf5ae9241de7fb57009d062
makjac <56642363+makjac@users.noreply.github.com>
2024-08-14 19:09:59 +0200

    feat: add shGetFileInfo method to FileInfoWindowsFfiTypes

--- lib/src/windows/file_info_windows_ffi_types.dart
+++ lib/src/windows/file_info_windows_ffi_types.dart
@@ -4,4 +4,6 @@ import 'package:ffi/ffi.dart';
 import 'package:win32/win32.dart';
 
 abstract class FileInfoWindowsFfiTypes {
+  int shGetFileInfo(Pointer<Utf16> filePath, int fileAttributes,
+      Pointer<SHFILEINFO> fileInfo, int fileInfoSize, int flags);
 }
