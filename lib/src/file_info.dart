ff26bac854c2726772f16a876fa4bf1c514bce2e
makjac <56642363+makjac@users.noreply.github.com>
2024-08-14 18:54:47 +0200

    teat: Add FlutterFileinfo package and abstract class FileInfo for platform interface

--- lib/src/file_info.dart
+++ lib/src/file_info.dart
@@ -1,4 +1,18 @@
+import 'package:flutter_file_info/flutter_file_info.dart';
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+
 abstract class FileInfo extends PlatformInterface {
-}
+  FileInfo() : super(token: _token);
+
+  static final Object _token = Object();
+
+  static FileInfo _instance = _DefaultFileInfo();
+
+  static FileInfo get instance => _instance;
+
+  static set instance(FileInfo instance) {
+    PlatformInterface.verifyToken(instance, _token);
+    _instance = instance;
+  }
 class _DefaultFileInfo extends FileInfo {
 }
