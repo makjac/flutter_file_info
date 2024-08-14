10423b6583139b029232fe03673ebeff0df6ff80
makjac <56642363+makjac@users.noreply.github.com>
2024-08-14 19:10:23 +0200

    feat: add getIconInfo method to FileInfoWindowsFfiTypes

--- lib/src/windows/file_info_windows_ffi_types.dart
+++ lib/src/windows/file_info_windows_ffi_types.dart
@@ -6,4 +6,6 @@ import 'package:win32/win32.dart';
 abstract class FileInfoWindowsFfiTypes {
   int shGetFileInfo(Pointer<Utf16> filePath, int fileAttributes,
       Pointer<SHFILEINFO> fileInfo, int fileInfoSize, int flags);
+
+  int getIconInfo(int hIcon, Pointer<ICONINFO> piconinfo);
 }
