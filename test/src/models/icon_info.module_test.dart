29f82baf982691df03a25cf0352a40ff5d52ab15
makjac <56642363+makjac@users.noreply.github.com>
2024-08-14 19:32:59 +0200

    test: add unit test for IconInfo copyWith method

--- test/src/models/icon_info.module_test.dart
+++ test/src/models/icon_info.module_test.dart
@@ -19,5 +19,27 @@ void main() {
       expect(iconInfo.colorDepth, 24);
       expect(iconInfo.pixelData, pixelData);
     });
+
+    test('IconInfo copyWith creates a new instance with updated values', () {
+      final pixelData = Uint8List.fromList([0, 1, 2, 3, 4]);
+      final newPixelData = Uint8List.fromList([5, 6, 7, 8, 9]);
+
+      final iconInfo = IconInfo(
+        width: 32,
+        height: 32,
+        colorDepth: 24,
+        pixelData: pixelData,
+      );
+
+      final updatedIconInfo = iconInfo.copyWith(
+        width: 64,
+        pixelData: newPixelData,
+      );
+
+      expect(updatedIconInfo.width, 64);
+      expect(updatedIconInfo.height, 32); // should remain the same
+      expect(updatedIconInfo.colorDepth, 24); // should remain the same
+      expect(updatedIconInfo.pixelData, newPixelData);
+    });
   });
 }
