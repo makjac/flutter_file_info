cd2aae617a6ab9392c3ab863bbd860d47bd78b73
makjac <56642363+makjac@users.noreply.github.com>
2024-08-14 19:14:36 +0200

    feat: implement FileInfoWindows class

---
+++ lib/src/windows/file_info_windows.dart
@@ -0,0 +1,15 @@
+import 'dart:ffi';
+import 'dart:typed_data';
+
+import 'package:ffi/ffi.dart';
+import 'package:flutter_file_info/flutter_file_info.dart';
+import 'package:flutter_file_info/src/windows/file_info_windows_ffi_types.dart';
+import 'package:win32/win32.dart';
+import 'package:image/image.dart' as img;
+
+class FileInfoWindows extends FileInfo {
+  FileInfoWindows({FileInfoWindowsFfiTypes? iconExtractor})
+      : _iconExtractor = iconExtractor ?? FileInfoWindowsFfiTypesImpl();
+
+  final FileInfoWindowsFfiTypes _iconExtractor;
+}
