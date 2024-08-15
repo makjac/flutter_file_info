2e0c91c04b2cc0f0e330e5d590033aae586f3dda
makjac <56642363+makjac@users.noreply.github.com>
2024-08-15 02:43:42 +0200

    docs: add documentation for getFileType method in WindowsUtils interface

--- lib/src/windows/windows_utils.dart
+++ lib/src/windows/windows_utils.dart
@@ -5,6 +5,25 @@ import 'package:flutter_file_info/src/windows/enum/file_attribiutes.dart';
 import 'package:win32/win32.dart';
 
 abstract class WindowsUtils {
+  /// Retrieves the type of a file based on its path using the Windows Shell API.
+  ///
+  /// This function takes a file path as input and uses the Windows Shell API to obtain information about the file,
+  /// including its type (e.g., 'Text Document', 'Image File'). The function allocates memory for a [SHFILEINFO] structure,
+  /// populates it with information using [SHGetFileInfo], and then extracts the type name from the structure.
+  ///
+  /// Example usage:
+  /// ```dart
+  /// String filePath = 'C:\\example\\document.txt';
+  /// String fileType = getFileType(filePath);
+  /// print('File Type: $fileType');
+  /// ```
+  ///
+  /// Note: The Windows Shell API is used to query file information, providing details such as the type name.
+  /// This function is specifically designed for Windows environments.
+  ///
+  /// See also:
+  /// - [SHFILEINFO structure documentation](https://docs.microsoft.com/en-us/windows/win32/api/shellapi/ns-shellapi-shfileinfow)
+  /// - [SHGetFileInfo function documentation](https://docs.microsoft.com/en-us/windows/win32/api/shellapi/nf-shellapi-shgetfileinfow)
   String getFileType(String filePath);
   DateTime convertFileTimeToDateTime(FILETIME fileTime);
   List<FileAttributes> getFileAttributesFromMask(int attributeMask);
