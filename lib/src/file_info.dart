2cf2193f29632e6a51afcb0dd038c84af5baa75b
makjac <56642363+makjac@users.noreply.github.com>
2024-08-15 02:42:41 +0200

    docs: add documentation for getFileIconInfo method in FileInfo interface

--- lib/src/file_info.dart
+++ lib/src/file_info.dart
@@ -15,6 +15,28 @@ abstract class FileInfo extends PlatformInterface {
     _instance = instance;
   }
 
+  /// Retrieves the icon information for a file or directory specified by [filePath].
+  ///
+  /// This method provides detailed information about the icon associated with
+  /// the file or directory, including the icon's dimensions, color depth, and
+  /// pixel data. The pixel data is encoded in PNG format.
+  ///
+  /// The [filePath] parameter should be a valid path to the file or directory
+  /// whose icon information is to be retrieved.
+  ///
+  /// Returns an [IconInfo] object containing details about the file's icon.
+  /// If the icon information cannot be retrieved, the method returns `null`.
+  ///
+  /// Throws an [UnimplementedError] if this method is not yet implemented.
+  ///
+  /// Example:
+  /// ```dart
+  /// final iconInfo = await fileInfoWindows.getFileIconInfo('C:\\path\\to\\file.txt');
+  /// if (iconInfo != null) {
+  ///   print('Icon width: ${iconInfo.width}');
+  ///   print('Icon height: ${iconInfo.height}');
+  /// }
+  /// ```
   Future<IconInfo?> getFileIconInfo(String filePath) =>
       throw UnimplementedError('getFileIconInfo() has not been implemented.');
   Future<FileMetadata?> getFileInfo(String filePath) =>
