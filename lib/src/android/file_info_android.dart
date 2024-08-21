ae02f6c734bfb6f1da57a9db1eafac640dfa55a1
makjac <56642363+makjac@users.noreply.github.com>
2024-08-21 20:09:35 +0200

    feat:  implement FileInfoAndroid for Android support

---
+++ lib/src/android/file_info_android.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_file_info/flutter_file_info.dart';
+import 'package:flutter_file_info/src/android/android_method_channel.dart';
+
+class FileInfoAndroid extends FileInfo {
+  FileInfoAndroid({AndroidMethodChannel? androidMethodChannel})
+      : _androidMethodChannel =
+            androidMethodChannel ?? AndroidMethodChannelImpl();
+
+  final AndroidMethodChannel _androidMethodChannel;
+
+  static void registerWith() {
+    FileInfo.instance = FileInfoAndroid();
+  }
+
+  @override
+  Future<IconInfo?> getFileIconInfo(String filePath) async {
+    return await _androidMethodChannel.getFileIcon(filePath);
+  }
+
+  @override
+  Future<FileMetadata?> getFileInfo(String filePath) =>
+      throw UnimplementedError('getFileInfo() has not been implemented.');
+}
