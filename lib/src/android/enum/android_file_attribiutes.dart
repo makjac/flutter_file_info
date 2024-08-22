4cc9338d51e9823bc9a4dc473fdd567b77ad71a1
makjac <56642363+makjac@users.noreply.github.com>
2024-08-22 07:19:59 +0200

    docs: add documentation for AndroidFileAttributes enum

--- lib/src/android/enum/android_file_attribiutes.dart
+++ lib/src/android/enum/android_file_attribiutes.dart
@@ -1,12 +1,42 @@
+// android_file_attributes.dart
+
+// ignore_for_file: constant_identifier_names
+
+/// Enumeration representing file attributes specific to the Android operating system.
+///
+/// This enum provides a set of constants that describe various permissions associated with files and directories on Android.
+///
+/// Usage:
+/// ```dart
+/// final attribute = AndroidFileAttributes.ownerWrite;
+/// print('Attribute: $attribute');
+/// ```
 enum AndroidFileAttributes {
+  /// Execute/search permission, group.
   GROUP_EXECUTE,
+
+  /// Read permission, group.
   GROUP_READ,
+
+  /// Write permission, group.
   GROUP_WRITE,
+
+  /// Execute/search permission, others.
   OTHERS_EXECUTE,
+
+  /// Read permission, others.
   OTHERS_READ,
+
+  /// Write permission, others.
   OTHERS_WRITE,
+
+  /// Execute/search permission, owner.
   OWNER_EXECUTE,
+
+  /// Read permission, owner.
   OWNER_READ,
+
+  /// Write permission, owner.
   OWNER_WRITE
 }
 class AndroidFileAttributesUtility {
