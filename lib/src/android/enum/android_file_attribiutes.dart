f7eed79a593f20d538682943550ff436e7eb2052
makjac <56642363+makjac@users.noreply.github.com>
2024-08-22 07:20:57 +0200

    docs: add documentation for AndroidFileAttributesUtility class and its parse method

--- lib/src/android/enum/android_file_attribiutes.dart
+++ lib/src/android/enum/android_file_attribiutes.dart
@@ -39,7 +39,36 @@ enum AndroidFileAttributes {
   /// Write permission, owner.
   OWNER_WRITE
 }
+
+/// Utility class for handling Android file attributes.
+///
+/// This class provides methods for working with Android file attributes, including converting
+/// attribute names from strings to their corresponding enum values.
+///
+/// Example usage:
+/// ```dart
+/// List<String> attributeNames = ['OWNER_WRITE', 'GROUP_READ'];
+/// Set<AndroidFileAttributes> attributes = AndroidFileAttributesUtility.parseAndroidFileAttributes(attributeNames);
+/// print(attributes); // Output: {AndroidFileAttributes.OWNER_WRITE, AndroidFileAttributes.GROUP_READ}
+/// ```
 class AndroidFileAttributesUtility {
+  /// Converts a list of attribute names in string format to a set of [AndroidFileAttributes].
+  ///
+  /// This method takes a list of attribute names as strings and converts each string to its corresponding
+  /// [AndroidFileAttributes] enum value. If a string does not match any of the defined attributes, it is ignored.
+  ///
+  /// [attributeNames] is a list of attribute names in string format. Each string should match one of the
+  /// enum names defined in [AndroidFileAttributes].
+  ///
+  /// Returns a set of [AndroidFileAttributes] corresponding to the provided attribute names. The result is
+  /// a set containing only valid attributes that were found in the input list.
+  ///
+  /// Example usage:
+  /// ```dart
+  /// List<String> names = ['OWNER_WRITE', 'GROUP_READ', 'INVALID'];
+  /// Set<AndroidFileAttributes> attributes = AndroidFileAttributesUtility.parseAndroidFileAttributes(names);
+  /// print(attributes); // Output: [AndroidFileAttributes.OWNER_WRITE, AndroidFileAttributes.GROUP_READ]
+  /// ```
   static Set<AndroidFileAttributes> parseAndroidFileAttributes(
       List<String> attributeNames) {
     return attributeNames
