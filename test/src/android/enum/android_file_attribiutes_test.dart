9be4f9a9bf4273adc1326a4a0d6ada74c2948809
makjac <56642363+makjac@users.noreply.github.com>
2024-08-22 07:26:06 +0200

    test: add test for parsing valid attribute names in AndroidFileAttributesUtility

---
+++ test/src/android/enum/android_file_attribiutes_test.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_file_info/flutter_file_info.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('AndroidFileAttributesUtility', () {
+    test('parses valid attribute names correctly', () {
+      final attributeNames = [
+        'GROUP_EXECUTE',
+        'GROUP_READ',
+        'GROUP_WRITE',
+        'OTHERS_EXECUTE',
+        'OTHERS_READ',
+        'OTHERS_WRITE',
+        'OWNER_EXECUTE',
+        'OWNER_READ',
+        'OWNER_WRITE'
+      ];
+
+      final result = AndroidFileAttributesUtility.parseAndroidFileAttributes(
+          attributeNames);
+
+      expect(
+          result,
+          containsAll([
+            AndroidFileAttributes.GROUP_EXECUTE,
+            AndroidFileAttributes.GROUP_READ,
+            AndroidFileAttributes.GROUP_WRITE,
+            AndroidFileAttributes.OTHERS_EXECUTE,
+            AndroidFileAttributes.OTHERS_READ,
+            AndroidFileAttributes.OTHERS_WRITE,
+            AndroidFileAttributes.OWNER_EXECUTE,
+            AndroidFileAttributes.OWNER_READ,
+            AndroidFileAttributes.OWNER_WRITE
+          ]));
+      expect(result.length, 9);
+    });
+  });
+}
