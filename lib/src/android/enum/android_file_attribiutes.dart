882419c66b2c53c9ffe2564e8283efe482b6f9b8
makjac <56642363+makjac@users.noreply.github.com>
2024-08-22 07:10:05 +0200

    feat: add AndroidFileAttributesUtility with parseAndroidFileAttributes method

--- lib/src/android/enum/android_file_attribiutes.dart
+++ lib/src/android/enum/android_file_attribiutes.dart
@@ -9,3 +9,35 @@ enum AndroidFileAttributes {
   OWNER_READ,
   OWNER_WRITE
 }
+class AndroidFileAttributesUtility {
+  static Set<AndroidFileAttributes> parseAndroidFileAttributes(
+      List<String> attributeNames) {
+    return attributeNames
+        .map((name) {
+          switch (name) {
+            case 'GROUP_EXECUTE':
+              return AndroidFileAttributes.GROUP_EXECUTE;
+            case 'GROUP_READ':
+              return AndroidFileAttributes.GROUP_READ;
+            case 'GROUP_WRITE':
+              return AndroidFileAttributes.GROUP_WRITE;
+            case 'OTHERS_EXECUTE':
+              return AndroidFileAttributes.OTHERS_EXECUTE;
+            case 'OTHERS_READ':
+              return AndroidFileAttributes.OTHERS_READ;
+            case 'OTHERS_WRITE':
+              return AndroidFileAttributes.OTHERS_WRITE;
+            case 'OWNER_EXECUTE':
+              return AndroidFileAttributes.OWNER_EXECUTE;
+            case 'OWNER_READ':
+              return AndroidFileAttributes.OWNER_READ;
+            case 'OWNER_WRITE':
+              return AndroidFileAttributes.OWNER_WRITE;
+            default:
+              return null;
+          }
+        })
+        .whereType<AndroidFileAttributes>()
+        .toSet();
+  }
+}
