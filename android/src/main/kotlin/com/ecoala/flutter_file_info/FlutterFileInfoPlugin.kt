fd68b9e5112f40a221d9cc8e2708ea2e04c63337
makjac <56642363+makjac@users.noreply.github.com>
2024-08-22 06:46:29 +0200

    feat: add handleGetFileInfo handler for file metadata retrieval in method channel

--- android/src/main/kotlin/com/ecoala/flutter_file_info/FlutterFileInfoPlugin.kt
+++ android/src/main/kotlin/com/ecoala/flutter_file_info/FlutterFileInfoPlugin.kt
@@ -1,10 +1,9 @@
 package com.ecoala.flutter_file_info
 
 import com.ecoala.flutter_file_info.FileIconProvider
-
+import com.ecoala.flutter_file_info.FileMetadataProvider
 
 import androidx.annotation.NonNull
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
@@ -16,26 +15,37 @@ import android.util.Base64
  * FlutterFileInfoPlugin class is responsible for handling method calls from Flutter and implementing the FlutterPlugin interface.
  */
 class FlutterFileInfoPlugin: FlutterPlugin, MethodCallHandler {
-  private lateinit var channel : MethodChannel
+  private lateinit var channel: MethodChannel
   private lateinit var fileIconProvider: FileIconProvider
+  private lateinit var fileMetadataProvider: FileMetadataProvider
 
   override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_file_info")
     channel.setMethodCallHandler(this)
 
     fileIconProvider = FileIconProvider(flutterPluginBinding.applicationContext, flutterPluginBinding.applicationContext.packageManager)
+    fileMetadataProvider = FileMetadataProvider()
   }
 
   override fun onMethodCall(call: MethodCall, result: Result) {
-    if (call.method == "getFileIcon") {
-      val filePath = call.argument<String>("filePath")
-      if (filePath != null) {
-        handleGetFileIcon(filePath, result)
-      } else {
-        result.error("INVALID_ARGUMENT", "File path is required", null)
+    when (call.method) {
+      "getFileIcon" -> {
+        val filePath = call.argument<String>("filePath")
+        if (filePath != null) {
+          handleGetFileIcon(filePath, result)
+        } else {
+          result.error("INVALID_ARGUMENT", "File path is required", null)
+        }
       }
-    } else {
-      result.notImplemented()
+      "getFileInfo" -> {
+        val filePath = call.argument<String>("filePath")
+        if (filePath != null) {
+          handleGetFileInfo(filePath, result)
+        } else {
+          result.error("INVALID_ARGUMENT", "File path is required", null)
+        }
+      }
+      else -> result.notImplemented()
     }
   }
 
@@ -52,13 +62,30 @@ class FlutterFileInfoPlugin: FlutterPlugin, MethodCallHandler {
       val encodedImage = Base64.encodeToString(pixelData, Base64.DEFAULT)
 
       val iconData = mapOf(
-              "pixelData" to encodedImage,
-              "width" to width,
-              "height" to height
+        "pixelData" to encodedImage,
+        "width" to width,
+        "height" to height
       )
       result.success(iconData)
     } else {
       result.error("FILE_NOT_FOUND", "Icon not found for the specified file", null)
+    }
+  }
+
+  /**
+   * Handles the retrieval of file metadata for the specified file path.
+   *
+   * @param filePath The path of the file for which to retrieve the metadata.
+   * @param result The result object used to communicate the file metadata back to the caller.
+   */
+  private fun handleGetFileInfo(filePath: String, result: Result) {
+    try {
+      val metadata = fileMetadataProvider.getFileMetadata(filePath)
+      val metadataMap = fileMetadataProvider.fileMetadataToMap(metadata)
+
+      result.success(metadataMap)
+    } catch (e: Exception) {
+      result.error("FILE_ERROR", "Failed to retrieve file metadata: ${e.message}", null)
     }
   }
 
