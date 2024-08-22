9d3ccd028c510fe6d8ef7d57e400357674bfcfcb
makjac <56642363+makjac@users.noreply.github.com>
2024-08-22 06:53:29 +0200

    docs(android): add Javadoc comments to FileMetadata class

--- android/src/main/kotlin/com/ecoala/flutter_file_info/FileMetadata.kt
+++ android/src/main/kotlin/com/ecoala/flutter_file_info/FileMetadata.kt
@@ -3,6 +3,20 @@ package com.ecoala.flutter_file_info
 import java.nio.file.attribute.PosixFilePermission
 import java.util.Date
 
+/**
+ * Represents metadata information about a file.
+ *
+ * @property filePath The path of the file.
+ * @property fileName The name of the file.
+ * @property fileExtension The extension of the file.
+ * @property fileType The type of the file.
+ * @property creationTime The creation time of the file.
+ * @property modifiedTime The last modified time of the file.
+ * @property accessedTime The last accessed time of the file.
+ * @property sizeBytes The size of the file in bytes.
+ * @property fileSize The size of the file as a human-readable string.
+ * @property attributes The set of POSIX file permissions for the file.
+ */
 data class FileMetadata(
     val filePath: String,
     val fileName: String?,
