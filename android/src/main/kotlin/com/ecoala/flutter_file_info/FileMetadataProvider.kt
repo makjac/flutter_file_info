9a1cd3655afbacc1f9dd3ea76e388e440569ef26
makjac <56642363+makjac@users.noreply.github.com>
2024-08-22 06:43:10 +0200

    feat(android): create FileMetadataProvider class for fetching file metadata

---
+++ android/src/main/kotlin/com/ecoala/flutter_file_info/FileMetadataProvider.kt
@@ -0,0 +1,14 @@
+package com.ecoala.flutter_file_info
+
+import android.annotation.TargetApi
+import android.os.Build
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.nio.file.attribute.BasicFileAttributes
+import java.nio.file.attribute.PosixFilePermission
+import java.text.DecimalFormat
+import java.util.*
+
+class FileMetadataProvider {
+}
