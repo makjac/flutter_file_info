d4a0c7b4d8c239abcf2de95d84f803013bd84e4e
makjac <maks.jackowski@proton.me>
2025-02-13 22:09:59 +0100

    feat(example): add initial macOS support with configuration files and entitlements

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_file_info
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterFileInfoPlugin.register(with: registry.registrar(forPlugin: "FlutterFileInfoPlugin"))
+}
