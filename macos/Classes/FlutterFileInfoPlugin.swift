0a32cb94b4d772cfc71517f56f84111dc116fb43
makjac <maks.jackowski@proton.me>
2025-02-13 19:38:42 +0100

    chore: enhance icon retrieval by resizing and returning dimensions

--- macos/Classes/FlutterFileInfoPlugin.swift
+++ macos/Classes/FlutterFileInfoPlugin.swift
@@ -1,10 +1,10 @@
-import Cocoa
-import FlutterMacOS
 import AppKit
+import FlutterMacOS
 
 public class FlutterFileInfoPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "flutter_file_info", binaryMessenger: registrar.messenger)
+    let channel = FlutterMethodChannel(
+      name: "flutter_file_info", binaryMessenger: registrar.messenger)
     let instance = FlutterFileInfoPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
@@ -12,33 +12,36 @@ public class FlutterFileInfoPlugin: NSObject, FlutterPlugin {
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     if call.method == "getFileIcon" {
       guard let args = call.arguments as? [String: Any],
-            let path = args["filePath"] as? String else {
-        result(FlutterError(
-          code: "INVALID_ARGS",
-          message: "Brak ścieżki pliku",
-          details: nil
-        ))
+        let path = args["filePath"] as? String
+      else {
+        result(FlutterError(code: "INVALID_ARGS", message: "Brak ścieżki pliku", details: nil))
         return
       }
 
       let url = URL(fileURLWithPath: path)
-
       let icon = NSWorkspace.shared.icon(forFile: url.path)
 
-      if let imageData = icon.tiffRepresentation,
-          let bitmapImage = NSBitmapImageRep(data: imageData),
-          let pngData = bitmapImage.representation(using: .png, properties: [:]) {
-        result(FlutterStandardTypedData(bytes: pngData))
-      } else {
-        result(FlutterError(
-          code: "NO_ICON",
-          message: "Nie udało się wygenerować ikony",
-          details: nil
-        ))
+      let newSize = NSSize(width: 256, height: 256)
+      let resizedIcon = self.resize(image: icon, to: newSize)
+
+      guard let tiffData = resizedIcon.tiffRepresentation,
+        let bitmapImage = NSBitmapImageRep(data: tiffData),
+        let pngData = bitmapImage.representation(using: .png, properties: [:])
+      else {
+        result(
+          FlutterError(code: "CONVERSION_ERROR", message: "Błąd konwersji ikony", details: nil))
+        return
       }
+
+      let response: [String: Any] = [
+        "pixelData": FlutterStandardTypedData(bytes: pngData),
+        "width": Int(icon.size.width),
+        "height": Int(icon.size.height),
+      ]
+
+      result(response)
     } else {
       result(FlutterMethodNotImplemented)
     }
   }
 }
-
