826246676597acbae2c484b09fa51fe1ddde2446
makjac <maks.jackowski@proton.me>
2025-02-14 01:36:05 +0100

    feat(ios): implement FileInfoIOS class for iOS file information handling

---
+++ lib/src/ios/file_info_ios.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_file_info/flutter_file_info.dart';
+import 'package:flutter_file_info/src/ios/ios_method_channel.dart';
+
+/// Represents a file information specific to iOS platform.
+class FileInfoIOS extends FileInfo {
+  FileInfoIOS({IOSMethodChannel? iOSMethodChannel})
+      : _iOSMethodChannel = iOSMethodChannel ?? IOSMethodChannelImpl();
+
+  final IOSMethodChannel _iOSMethodChannel;
+
+  static void registerWith() {
+    FileInfo.instance = FileInfoIOS();
+  }
+
+  @override
+  Future<IconInfo?> getFileIconInfo(String filePath) async {
+    return await _iOSMethodChannel.getFileIcon(filePath);
+  }
+
+  @override
+  Future<FileMetadata?> getFileInfo(String filePath) async {
+    throw UnimplementedError('getFileInfo() has not been implemented.');
+    // return await _iOSMethodChannel.getFileInfo(filePath);
+  }
+}
