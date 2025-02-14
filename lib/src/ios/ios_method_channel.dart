6b115e675f8f494fd5b37d71e63260ae2c088ddf
makjac <maks.jackowski@proton.me>
2025-02-14 03:13:12 +0100

    feat(ios): update getFileInfo method to return nullable FileMetadata and handle exceptions

--- lib/src/ios/ios_method_channel.dart
+++ lib/src/ios/ios_method_channel.dart
@@ -1,8 +1,7 @@
-import 'dart:convert';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_file_info/flutter_file_info.dart';
+import 'package:flutter_file_info/src/ios/enum/ios_file_attribute.dart';
 
 /// Represents an abstract class for an iOS method channel.
 ///
@@ -18,7 +17,7 @@ abstract class IOSMethodChannel {
   /// Retrieves the file metadata for a file located at the specified [filePath].
   ///
   /// Returns a [Future] that completes with a [FileMetadata] object containing the file metadata.
-  Future<FileMetadata> getFileInfo(String filePath);
+  Future<FileMetadata?> getFileInfo(String filePath);
 }
 
 class IOSMethodChannelImpl implements IOSMethodChannel {
@@ -45,28 +44,44 @@ class IOSMethodChannelImpl implements IOSMethodChannel {
   }
 
   @override
-  Future<FileMetadata> getFileInfo(String filePath) async {
-    final result =
-        await methodChannel.invokeMethod('getFileInfo', {'filePath': filePath});
+  Future<FileMetadata?> getFileInfo(String filePath) async {
+    try {
+      final result = await methodChannel
+          .invokeMethod('getFileInfo', {'filePath': filePath});
 
-    final Map<String, dynamic> data = Map<String, dynamic>.from(result);
+      final Map<String, dynamic> data = Map<String, dynamic>.from(result);
 
-    return FileMetadata(
-      filePath: data['filePath'] as String,
-      fileName: data['fileName'] as String?,
-      fileExtension: data['fileExtension'] as String?,
-      fileType: data['fileType'] as String?,
-      creationTime: data['creationTime'] != null
-          ? DateTime.fromMillisecondsSinceEpoch(data['creationTime'] as int)
-          : null,
-      modifiedTime: data['modifiedTime'] != null
-          ? DateTime.fromMillisecondsSinceEpoch(data['modifiedTime'] as int)
-          : null,
-      accessedTime: data['accessedTime'] != null
-          ? DateTime.fromMillisecondsSinceEpoch(data['accessedTime'] as int)
-          : null,
-      sizeBytes: data['sizeBytes'] as int?,
-      fileSize: data['fileSize'] as String?,
-    );
+      return FileMetadata(
+        filePath: data['filePath'] as String,
+        fileName: data['fileName'] as String?,
+        fileExtension: data['fileExtension'] as String?,
+        fileType: data['fileType'] as String?,
+        creationTime: data['creationTime'] != null
+            ? DateTime.fromMillisecondsSinceEpoch(data['creationTime'] as int)
+            : null,
+        modifiedTime: data['modifiedTime'] != null
+            ? DateTime.fromMillisecondsSinceEpoch(data['modifiedTime'] as int)
+            : null,
+        accessedTime: data['accessedTime'] != null
+            ? DateTime.fromMillisecondsSinceEpoch(data['accessedTime'] as int)
+            : null,
+        sizeBytes: data['sizeBytes'] as int?,
+        fileSize: data['fileSize'] as String?,
+        iosAttributes: _parseIOSAttributes(
+          List<String>.from(data['iOSAttributes'] as List<dynamic>),
+        ),
+      );
+    } on PlatformException catch (e) {
+      debugPrint('Error: ${e.message}');
+      return null;
+    }
+  }
+
+  List<IosFileAttribute> _parseIOSAttributes(List<String> attributes) {
+    return attributes.map((attr) {
+      return IosFileAttribute.values.firstWhere((element) {
+        return element.name == attr;
+      });
+    }).toList();
   }
 }
